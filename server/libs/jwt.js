92378b70fc82fa8889a064897e0ada875dd9bcc2
Part.e <part.emxteam@gmail.com>
2024-08-16 16:30:11 -0600

    v1.0.18

--- server/libs/jwt.js
+++ server/libs/jwt.js
@@ -1,12 +1,12 @@
-// import { TOKEN_SECRET } from "../config.js"
-import dotenv from "dotenv";
+import { TOKEN_SECRET } from "../config.js"
+// import dotenv from "dotenv";
 import jwt from 'jsonwebtoken';
 
-dotenv.config();
+// dotenv.config();
 
 export function createAccessToken(payload){
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "1d" } ,
+        jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" } ,
             (err,token) => {
                 if(err) reject(err)
                 resolve(token)
