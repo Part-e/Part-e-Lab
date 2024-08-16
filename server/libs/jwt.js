00e101bf3cc0178efff3df19ce249b5b98b32a3a
Part.e <part.emxteam@gmail.com>
2024-08-16 13:12:57 -0600

    v1.0.17

--- server/libs/jwt.js
+++ server/libs/jwt.js
@@ -1,9 +1,12 @@
-import { TOKEN_SECRET } from "../config.js"
+// import { TOKEN_SECRET } from "../config.js"
+import dotenv from "dotenv";
 import jwt from 'jsonwebtoken';
+
+dotenv.config();
 
 export function createAccessToken(payload){
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" } ,
+        jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "1d" } ,
             (err,token) => {
                 if(err) reject(err)
                 resolve(token)
--- server/middlewares/validateToken.js
+++ server/middlewares/validateToken.js
@@ -1,12 +1,15 @@
 import jwt from 'jsonwebtoken';
-import { TOKEN_SECRET } from '../config.js';
+import dotenv from "dotenv";
+// import { TOKEN_SECRET } from '../config.js';
+
+dotenv.config();
 
 export const authRequired = (req, res, next) => { 
     const {token} = req.cookies;
 
     if(!token) return res.status(401).json({message: 'No token, authorization denied'});
     
-    jwt.verify(token, TOKEN_SECRET, (error, user) => {
+    jwt.verify(token, process.env.TOKEN_SECRET, (error, user) => {
         if (error) return res.status(403).json({message: 'Invalid token'});
 
         req.user = user
